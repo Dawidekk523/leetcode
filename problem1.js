8369496a86365aa27e4b7a77890cf72587fa55bc
dawidekk523 <daveedsgn@gmail.com>
2023-04-12 04:00:51 +0200

    if statement to fix

--- problem1.js
+++ problem1.js
@@ -23,16 +23,40 @@ const romToNum = {
         break;
     }
   },
+  rem: (e) => {
+    switch (e) {
+      case "I":
+        romToNum.sum = romToNum.sum - 1;
+        break;
+      case "V":
+        romToNum.sum = romToNum.sum - 5;
+        break;
+      case "X":
+        romToNum.sum = romToNum.sum - 10;
+        break;
+
+      default:
+        break;
+    }
+  },
   math: (input) => {
-    for (let index = 1; index < input.length; index++) {
+    for (let index = 0; index < input.length; index++) {
       let currNumber = input[index];
       let lastNumber = input[index - 1];
-      if (currNumber == lastNumber) {
-        console.log(input[index]);
+      if (currNumber == lastNumber || lastNumber == undefined) {
+        console.log("dodaje " + currNumber);
         romToNum.add(input[index]);
+      } else {
+        if (currNumber > lastNumber) {
+          romToNum.add(currNumber);
+          console.log("dodaje " + currNumber);
+        } else {
+          romToNum.rem(currNumber);
+          console.log("odejmuje " + currNumber);
+        }
       }
     }
   },
 };
 
-romToNum.numbersToArr("XXXXI");
+romToNum.numbersToArr("XIX");
