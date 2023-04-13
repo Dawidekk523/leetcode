29efaa19e52952e9e4064f246869933223079ec2
dawidekk523 <daveedsgn@gmail.com>
2023-04-13 19:18:08 +0200

    done

--- problem5.js
+++ problem5.js
@@ -4,12 +4,13 @@
  */
 var isValid = function (s) {
   let sSplited = s.split("");
-  for (let index = 0; index < sSplited.length; index++) {
+  for (let index = 0; index < sSplited.length + 1; index++) {
     switch (sSplited[index]) {
       case "(":
         if (sSplited[index + 1] == ")") {
           index++;
         } else {
+          console.log("false");
           return false;
         }
         break;
@@ -17,6 +18,7 @@ var isValid = function (s) {
         if (sSplited[index + 1] == "}") {
           index++;
         } else {
+          console.log("false");
           return false;
         }
         break;
@@ -24,11 +26,15 @@ var isValid = function (s) {
         if (sSplited[index + 1] == "]") {
           index++;
         } else {
+          console.log("false");
           return false;
         }
         break;
+      case undefined:
+        console.log("true");
+        return true;
     }
   }
 };
 
-isValid("{}(][]");
+isValid("{}()");
